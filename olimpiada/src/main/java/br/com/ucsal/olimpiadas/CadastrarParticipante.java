dbf8ff25fb446ba240fe50f3e7e41b9c723eb0c4
Marcomira12 <129343317+Marcomira12@users.noreply.github.com>
2026-03-06 17:05:34 -0300

    refatoração e inclusão classe cadastras participante

---
+++ olimpiada/src/main/java/br/com/ucsal/olimpiadas/CadastrarParticipante.java
@@ -0,0 +1,36 @@
+package br.com.ucsal.olimpiadas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CadastrarParticipante {
+	private int proximoParticipanteId = 1;
+
+	public int getProximoParticipanteId() {
+		return proximoParticipanteId;
+	}
+
+
+	List<Participante> participantes = new ArrayList<>();
+
+	public void cadastrarParticipante(Scanner in) {
+		System.out.print("Nome: ");
+		var nome = in.nextLine();
+
+		System.out.print("Email (opcional): ");
+		var email = in.nextLine();
+
+		if (nome == null || nome.isBlank()) {
+			System.out.println("nome inválido");
+			return;
+		}
+		Participante p = new Participante();
+		p.setId(getProximoParticipanteId() + 1);
+		p.setNome(nome);
+		p.setEmail(email);
+		participantes.add(p);
+
+		System.out.println("Participante cadastrado: " + p);
+	}
+}
---
+++ olimpiada/src/main/java/br/com/ucsal/olimpiadas/Participante.java
@@ -0,0 +1,45 @@
+package br.com.ucsal.olimpiadas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Participante {
+	private int id;
+	private String nome;
+	private String email;
+	
+	
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return " [id=" + id + ", nome=" + nome + ", email=" + email + "]";
+	}
+	
+	
+	
+}
