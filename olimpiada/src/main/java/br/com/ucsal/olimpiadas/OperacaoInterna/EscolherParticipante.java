fdb2748ff82676c90254afffb2940f09fe78fe2f
MARCO ANTONIO DO BOMFIM MIRA <200037033@UCSAL.BR>
2026-03-10 21:15:51 -0300

    adicionando lista dinamica para utilização nas demais classes e corrigindo a classe seed e escolherParticipante

--- olimpiada/src/main/java/br/com/ucsal/olimpiadas/OperacaoInterna/EscolherParticipante.java
+++ olimpiada/src/main/java/br/com/ucsal/olimpiadas/OperacaoInterna/EscolherParticipante.java
@@ -1,24 +1,20 @@
 package br.com.ucsal.olimpiadas.OperacaoInterna;
+
 import java.util.Scanner;
 
 import br.com.ucsal.olimpiadas.Operacao.Factory;
 
 public class EscolherParticipante {
-	private Factory f;
 
-	public EscolherParticipante(Factory f) {
-		this.f = f;
-	}
-
-	public Long escolherParticipante(Scanner in) {
+	public Integer escolherParticipante(Factory f) {
 		System.out.println("\nParticipantes:");
-		for (var p : f.getCadastrar().getParticipantes()) {
+		for (var p : f.getCadastrar().getLista()) {
 			System.out.printf("  %d) %s%n", p.getId(), p.getNome());
 		}
 		System.out.print("Escolha o id do participante: ");
 
 		try {
-			long id = Long.parseLong(in.nextLine());
+			int id = f.getIn().nextInt();
 			boolean existe = f.getCadastrar().getParticipantes().stream().anyMatch(p -> p.getId() == id);
 			if (!existe) {
 				System.out.println("id inválido");
@@ -30,6 +26,4 @@ public class EscolherParticipante {
 			return null;
 		}
 	}
-
-	
 }
